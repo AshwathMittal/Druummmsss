28cfce9f79b517f6b529652d38acae429acc7cec
AshwathMittal <68093378+AshwathMittal@users.noreply.github.com>
2020-09-29 20:02:57 +0530

    Add files via upload

---
+++ Drum Kit Completed/index.js
@@ -0,0 +1,35 @@
+function w() {
+  var tom1 = new Audio("sounds/tom-1.mp3");
+  tom1.play();
+}
+
+function a() {
+  var tom2 = new Audio("sounds/tom-2.mp3");
+  tom2.play();
+}
+
+function s() {
+  var tom3 = new Audio('sounds/tom-3.mp3');
+  tom3.play();
+}
+
+function d() {
+  var tom4 = new Audio('sounds/tom-4.mp3');
+  tom4.play();
+}
+
+function j() {
+  var snare = new Audio('sounds/snare.mp3');
+  snare.play();
+}
+
+function k() {
+  var crash = new Audio('sounds/crash.mp3');
+  crash.play();
+}
+
+
+function l() {
+  var kick = new Audio('sounds/kick-bass.mp3');
+  kick.play();
+}
